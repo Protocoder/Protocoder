b3bbb2d58e49b8579da9e3c322244be90cccf5c4
Gopi Palaniappan <gopinath@motorola.com>
2013-07-17 18:38:13 -0700

    Adding apis for ui, android, ioio

--- AndroidApp/MakeWithMoto/src/com/makewithmoto/apprunner/JInterface.java
+++ AndroidApp/MakeWithMoto/src/com/makewithmoto/apprunner/JInterface.java
@@ -1,7 +1,9 @@
 package com.makewithmoto.apprunner;
 
 import java.lang.ref.WeakReference;
+
 import android.app.Activity;
+
 
 public class JInterface {
 	
@@ -14,6 +16,18 @@ public class JInterface {
 		this.c = new WeakReference<AppRunnerActivity>((AppRunnerActivity) appActivity); 
 
 	}
+	
+	public void callback(String fn){
+		
+		try{
+			   c.get().interpreter.callJsFunction(fn,"");
+		}catch (Throwable e){
+
+		           // TODO
+		}
+		
+	}
+	
 	
 	public void destroy() {
 	}
