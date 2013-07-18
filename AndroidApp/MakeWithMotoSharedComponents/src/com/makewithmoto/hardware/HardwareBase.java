a1d9aee73a3ead3ce289679cb924a037892e0ff4
Gopi Palaniappan <gopinath@motorola.com>
2013-07-18 00:47:27 -0700

    Added more APIs to the list and moved the jar file dependencies over

---
+++ AndroidApp/MakeWithMotoSharedComponents/src/com/makewithmoto/hardware/HardwareBase.java
@@ -0,0 +1,29 @@
+package com.makewithmoto.hardware;
+
+import com.makewithmoto.hardware.HardwareCallback;
+
+public class HardwareBase {
+	protected HardwareCallback callback_;
+	
+	public HardwareBase(HardwareCallback callback) {
+		this.callback_ = callback;
+	}
+	/**
+	 * Power on the hardware board
+	 */
+	public void powerOn() {
+	}
+	
+	/**
+	 * Power off the hardware board
+	 */
+	public void powerOff() {
+	}
+	
+	/**
+	 * Erase internal memory of the board
+	 */
+	public void erase() {
+	}
+
+}
