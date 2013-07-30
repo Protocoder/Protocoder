cfd0795c5ec818b7383f287fafafa65d2cd7229a
ncbq76 <ncbq76@motorola.com>
2013-07-29 22:37:49 -0700

    Adding full sample code and Switch widget
    HSHIEH
    
    - Adding full sample code and new widget

--- AndroidApp/MakeWithMoto/assets/examples/AllWidgets/script.js
+++ AndroidApp/MakeWithMoto/assets/examples/AllWidgets/script.js
@@ -1,6 +1,7 @@
 //Set up the canvas with padding all around and a white background
 ui.setPadding(16, 16, 16, 16);
 ui.backgroundColor(255, 255, 255);
+//ui.backgroundImage("/sdcard/makewithmoto/projects/AllWidgets/makewithmotologo.png");
 
 //Add a generic button
 ui.button("Button", 0, 0,500,100, function(){android.vibrate(500);});
@@ -22,3 +23,5 @@ ui.image(0, 1050, 300, 300, "/sdcard/makewithmoto/projects/AllWidgets/makewithmo
 ui.imagebutton(400, 1050, 300, 300,"/sdcard/makewithmoto/projects/AllWidgets/makewithmotologo.png", false, function(){android.vibrate(500);});
 //Add an image loaded from the web
 ui.webimage(0, 1400, 500, 500, "http://images.latinospost.com/data/images/full/22151/pokemon.jpg");
+//Add an image loaded from the web
+ui.toggleswitch(0, 2000, 500, 100, true, function(){android.vibrate(500);});
