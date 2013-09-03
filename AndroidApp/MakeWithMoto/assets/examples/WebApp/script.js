b555f8f85210db5481fb880f1815ed10f2fd4a3d
Victor Diaz <victormdb@gmail.com>
2013-09-02 20:42:05 -0400

    added new examples, speech recognition, fixed error when creating new app, removed old examples and old webapp

--- AndroidApp/MakeWithMoto/assets/examples/WebApp/script.js
+++ AndroidApp/MakeWithMoto/assets/examples/WebApp/script.js
@@ -4,11 +4,24 @@ ui.button("Add plot", 0, 0,500,100, function(){
     plot = webapp.addPlot("w", 100, 100, 100, 100);
 });
 
-ui.button("Update", 0, 200,500,100, function(){
+ui.button("Update", 0, 100,500,100, function(){
     plot.update(10 * Math.random());
 });
 
 
-ui.toggleButton("Show Hide dashboard", 0, 400,500,100,false, function(b){
+ui.toggleButton("Show Hide dashboard", 0, 200,500,100,false, function(b){
     webapp.showDashboard(b);
 });
+
+//labels need an unique id in order to be identified in the webapp
+var label = webapp.addLabel("id1", "default_value", 100, 100, 200, 100);
+
+ui.button("hola", 0, 300,500,100, function(){
+    label.setText("hola");
+
+});
+
+ui.button("adios", 0, 400,500,100, function(){
+    label.setText("adios");
+});
+
--- AndroidApp/MakeWithMoto/assets/oldexamples/Toast/script.js
+++
@@ -1,6 +0,0 @@
-// Toast example program.
-var Toast = Packages.android.widget.Toast;
-Toast.makeText(
-     Activity,
-     "Hello World!", 
-     Toast.LENGTH_SHORT).show();
