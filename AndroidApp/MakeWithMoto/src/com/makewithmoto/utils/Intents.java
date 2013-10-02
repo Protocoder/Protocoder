9397d4f933b95751a37dcb5d43ba41f17e89b536
Victor Diaz <victormdb@gmail.com>
2013-10-02 05:06:33 -0400

    add licenses

--- AndroidApp/MakeWithMoto/src/com/makewithmoto/utils/Intents.java
+++ AndroidApp/MakeWithMoto/src/com/makewithmoto/utils/Intents.java
@@ -1,13 +1,38 @@
+/*
+ * Protocoder 
+ * A prototyping platform for Android devices 
+ * 
+ * 
+ * Copyright (C) 2013 Motorola Mobility LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions: 
+ * 
+ * The above copyright notice and this permission notice shall be included in all 
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+ * THE SOFTWARE.
+ * 
+ */
+
 package com.makewithmoto.utils;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -18,10 +43,7 @@ import android.provider.MediaStore;
 
 public class Intents {
 
-	public static final String TYPE_SOUNDCLOUD = "com.soundcloud.android.SHARE";
-	public static final String TYPE_DROPBOX = "";
-	public static final String TYPE_GMAIL = "";
-	public static final String TYPE_MAILCLIENT = "";
+
 
 	/**
 	 * Indicates whether the specified action can be used as an intent. This
@@ -89,25 +111,7 @@ public class Intents {
     ad.show();
   }	
 	
-
-	static public void shareWithSoundCloud(Activity a, String audioFileURL) {
-
-		// this is the audio file we want to upload
-		File myAudiofile = new File(audioFileURL);
-		Intent intent = new Intent(TYPE_SOUNDCLOUD).putExtra(
-				Intent.EXTRA_STREAM, Uri.fromFile(myAudiofile)).putExtra(
-				"com.soundcloud.android.extra.title", "Demo");
-		// more metadata can be set, see below
-
-		try {
-			// takes the user to the SoundCloud sharing screen
-			a.startActivityForResult(intent, 0);
-		} catch (ActivityNotFoundException e) {
-			// SoundCloud Android app not installed, show a dialog etc.
-		}
-
-	}
-
+  
 	static public void shareWithMail(Context context, String emailTo,
 			String emailCC, String subject, String emailText,
 			List<String> filePaths) {
