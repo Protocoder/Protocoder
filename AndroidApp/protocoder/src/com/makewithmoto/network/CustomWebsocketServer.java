51e6ac8bb5b7779d2784c94d211be5a9fea79c93
Victor Diaz <victormdb@gmail.com>
2013-10-05 10:18:27 -0400

    changed README for hackMIT

--- AndroidApp/protocoder/src/com/makewithmoto/network/CustomWebsocketServer.java
+++ AndroidApp/protocoder/src/com/makewithmoto/network/CustomWebsocketServer.java
@@ -33,7 +33,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Vector;
 
 import org.java_websocket.WebSocket;
 import org.java_websocket.drafts.Draft;
