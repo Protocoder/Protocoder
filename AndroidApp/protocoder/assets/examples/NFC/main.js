25e25fbe30e1edddd3fedf45913b28f2dfaee757
Victor Diaz <victormdb@gmail.com>
2013-11-26 10:32:23 -0800

    nfc now reads and writes data, not fully supported though

--- AndroidApp/protocoder/assets/examples/NFC/main.js
+++ AndroidApp/protocoder/assets/examples/NFC/main.js
@@ -2,9 +2,20 @@
 *	Shows which id and message are written in the NFC tag 
 */
 
-var nfcdata = ui.addLabel("", 20, 20, 500, 100, 16);
+var nfcinfo = ui.addLabel("", 20, 20, 500, 100, 16);
 
 sensors.onNFC(function (id, data) { 
     console.log("the nfc id is: " + id, data); 
-    nfcdata.setText(id + " " + data)
+    nfcinfo.setText(id + " " + data)
 });
+
+
+//when we click 
+//the next touched nfc will be written with the data 
+ui.addButton("Write to NFC", 0, 0,500,100, function(){
+	sensors.writeNFC("this is a test", function() {
+		nfcinfo.setText("data written");
+	});
+});
+
+
