ec10723a201f8194561def30fa977af6aaa22613
Victor Diaz <victormdb@gmail.com>
2014-07-20 14:10:10 +0200

    moved calculatedColor function to AndroidUtils

--- protocoder/protocoder_app/src/main/java/org/protocoder/utils/AndroidUtils.java
+++ protocoder/protocoder_app/src/main/java/org/protocoder/utils/AndroidUtils.java
@@ -207,4 +207,23 @@ public class AndroidUtils {
     public static boolean isVersionMinSupported() {
        return AppSettings.MIN_SUPPORTED_VERSION > Build.VERSION.SDK_INT;
     }
+
+    public static int calculateColor(float fraction, int startValue, int endValue) {
+
+        int startInt = startValue;
+        int startA = (startInt >> 24) & 0xff;
+        int startR = (startInt >> 16) & 0xff;
+        int startG = (startInt >> 8) & 0xff;
+        int startB = startInt & 0xff;
+
+        int endInt = endValue;
+        int endA = (endInt >> 24) & 0xff;
+        int endR = (endInt >> 16) & 0xff;
+        int endG = (endInt >> 8) & 0xff;
+        int endB = endInt & 0xff;
+
+        return (startA + (int) (fraction * (endA - startA))) << 24
+                | (startR + (int) (fraction * (endR - startR))) << 16
+                | (startG + (int) (fraction * (endG - startG))) << 8 | ((startB + (int) (fraction * (endB - startB))));
+    }
 }
