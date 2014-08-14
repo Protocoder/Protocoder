7ae934012aaecd4ffc59871c65663b120132cf02
taseenb <esteban.almiron@gmail.com>
2014-08-15 00:04:25 +0200

    improvements to the backbone app (added CommunicationController and views)

--- protocoder/protocoder_app/src/main/assets/webapp_bb/src/views/uiView.js
+++ protocoder/protocoder_app/src/main/assets/webapp_bb/src/views/uiView.js
@@ -6,23 +6,35 @@ define([
 
   'use strict';
 
-  var Ui = Backbone.View.extend({
+  var UiView = Backbone.View.extend({
 
     events: {
       "click .ui": "clicked"
     },
 
-    init: function () {
+    /**
+     * Init.
+     * @returns {Ui}
+     */
+    initialize: function () {
 
       console.log("Welcome to the Ui view!");
 
       return this;
     },
+
+    /**
+     * Render the view (add dom elements or html to the page).
+     * @returns {Ui}
+     */
     render: function () {
 
+      // Prepare html
       var tpl = _.template(uiHtml);
+      var tplHtml = tpl({a: 1000});
 
-      console.log(tpl({a: 1000}));
+      // Inject html (at once for performance)
+      $('#example_for_victor').html(tplHtml);
 
       return this;
     },
@@ -35,6 +47,6 @@ define([
     }
   });
 
-  return Ui;
+  return UiView;
 
 });
