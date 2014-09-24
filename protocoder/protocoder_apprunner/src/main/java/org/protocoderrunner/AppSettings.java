c32f017a3003368d1e1ed90a72b7d0dab78d1333
Victor Diaz <victormdb@gmail.com>
2014-09-25 01:04:33 +0200

    change constants settings to static

--- protocoder/protocoder_apprunner/src/main/java/org/protocoderrunner/AppSettings.java
+++ protocoder/protocoder_apprunner/src/main/java/org/protocoderrunner/AppSettings.java
@@ -34,16 +34,16 @@ import android.os.Build;
 public class AppSettings {
 
 	// == APP SETTINGS ==========
-	public static boolean DEBUG = true;
-	public static boolean FULLSCREEN = false;
-	public static boolean PORTRAIT = false;
-	public static boolean STAY_AWAKE = false;
-	public static boolean OVERRIDE_HOME_BUTTONS = false;
-	public static boolean OVERRIDE_VOLUME_BUTTONS = false;
-	public static boolean HIDE_HOME_BAR = false;
-	public static boolean SCREEN_ALWAYS_ON = false;
-	public static boolean CLOSE_WITH_BACK = true;
-	public static boolean STANDALONE = false;
+	public final static boolean DEBUG = true;
+	public final static boolean FULLSCREEN = false;
+	public final static boolean PORTRAIT = false;
+	public final static boolean STAY_AWAKE = false;
+	public final static boolean OVERRIDE_HOME_BUTTONS = false;
+	public final static boolean OVERRIDE_VOLUME_BUTTONS = false;
+	public final static boolean HIDE_HOME_BAR = false;
+	public final static boolean SCREEN_ALWAYS_ON = false;
+	public final static boolean CLOSE_WITH_BACK = true;
+	public final static boolean STANDALONE = false;
 
 	public static int MIN_SUPPORTED_VERSION = Build.VERSION_CODES.ICE_CREAM_SANDWICH;
 
