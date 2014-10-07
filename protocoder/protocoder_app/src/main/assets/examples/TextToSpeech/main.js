d0b016430c308862488149e05815f38d3e33c2a0
Victor Diaz <victormdb@gmail.com>
2014-10-07 03:39:34 +0200

    modified examples

--- protocoder/protocoder_app/src/main/assets/examples/TextToSpeech/main.js
+++ protocoder/protocoder_app/src/main/assets/examples/TextToSpeech/main.js
@@ -2,6 +2,6 @@
 *	Uses android text to speech 
 */ 
 
-ui.addButton("Speak!", 0, 0, 500, 100, function() { 
+ui.addButton("Speak!", 0, 0, 500, 200, function() { 
 	media.textToSpeech("hola amigos"); 
 });
