3575f0be0544b287095cff1402c97c8349e3d72f
Victor Diaz <victormdb@gmail.com>
2014-10-16 01:26:09 +0200

    fixed websocket example

--- protocoder/protocoder_app/src/main/assets/examples/Network_Websockets/main.js
+++ protocoder/protocoder_app/src/main/assets/examples/Network_Websockets/main.js
@@ -7,21 +7,23 @@
 var port= 2525; 
 
 var server = network.startWebsocketServer(port, function(status, socket, data) {
-	console.log("server: ", status, socket.getLocalSocketAddress(), data);
-	if (status == "message") {
-		client.send("pong");
+	console.log("server:", status, socket.getLocalSocketAddress(), data);
+	if (status == "onMessage") {
+		socket.send("pong");
 	}
 });
 
 var url = "ws://127.0.0.1:2525";
-var client = network.connectWebsocket(uri, function(status, data) {
-	console.log(status, data);
+var client = network.connectWebsocket(url, function(status, data) {
+	console.log("client:", status, data);
 });
+console.log(client);
 
 ui.addButton("send data", 10, 10, 200, 200, function() {
-	client.send("client ", "ping!");
+	client.send("ping!");
 });
 
 //other useful methods 
 //server.stop();
 //client.close();
+
