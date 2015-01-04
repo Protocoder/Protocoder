1bffdf99ec71fb9fcbfb3692c2b31cafbe5e7145
Victor Diaz <victormdb@gmail.com>
2015-01-04 08:12:39 +0100

    merget plotview files

--- protocoder/protocoder_apprunner/src/main/java/org/protocoderrunner/apprunner/api/widgets/PPlotView.java
+++ protocoder/protocoder_apprunner/src/main/java/org/protocoderrunner/apprunner/api/widgets/PPlotView.java
@@ -30,104 +30,367 @@
 package org.protocoderrunner.apprunner.api.widgets;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
 
 import org.protocoderrunner.apidoc.annotation.APIMethod;
 import org.protocoderrunner.apidoc.annotation.APIParam;
 import org.protocoderrunner.apprunner.ProtocoderScript;
-import org.protocoderrunner.views.PlotView;
+import org.protocoderrunner.utils.MLog;
+import org.protocoderrunner.views.CanvasUtils;
 
-public class PPlotView extends PlotView implements PViewInterface {
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Vector;
+
+public class PPlotView extends View implements PViewInterface {
 
 	private static final String TAG = "PPlotView";
 
-	public PPlotView(Context context) {
-		super(context);
-	}
+    // paint
+    private final Paint mPaint = new Paint();
+    private Canvas mCanvas = new Canvas();
+
+    //store each plot values
+    HashMap<String, Plot> plots = new HashMap<String, Plot>();
+
+    // util data
+    private float mLastX;
+    private float mMaxValue;
+    private float mMinValue;
+
+    // widget size
+    private float mWidth;
+    private float mHeight;
+    private int mNumPoints;
+    private final int mCurrentPosition = 0;
+    private int mDefinition = 2;
+    private float mMinBoundary;
+    private float mMaxBoundary;
+    private boolean mReady = false;
+    private float thickness = 2;
+
+    public PPlotView(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+        init();
+    }
+
+    public PPlotView(Context context) {
+        super(context);
+        init();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+
+
+    public void init() {
+        mDefinition = 5;
+
+        mPaint.setStrokeWidth(thickness);
+
+        if (isInEditMode()) {
+            loadDemoValues();
+        }
+    }
+
+    public void loadDemoValues() {
+        Plot plot1 = new Plot(Color.RED);
+        addPlot("", plot1);
+        mNumPoints = 1000; // (int)(getWidth() / mDefinition);
+        for (int i = 0; i < mNumPoints; i++) {
+            plot1.plotValues.add(((float) Math.random() * 220));
+        }
+    }
+
+
+    public void addPlot(String plotName, Plot plot) {
+        plots.put(plotName, plot);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+
+        mWidth = w;
+        mHeight = h;
+
+        mNumPoints = (int) (mWidth / mDefinition);
+
+        // zeroed the plot
+        Iterator it = plots.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry m = (Map.Entry) it.next();
+            // System.out.println(pairs.getKey() + " = " + pairs.getValue());
+            // it.remove(); // avoids mContext ConcurrentModificationException
+            Plot p = (Plot) m.getValue();
+            p.zero();
+        }
+
+        if (mWidth < mHeight) {
+            mMaxValue = w;
+        } else {
+            mMaxValue = w - 50;
+        }
+
+        mLastX = mMaxValue;
+
+        mCanvas = new Canvas();
+
+        mPaint.setColor(0x88000000);
+        mPaint.setStyle(Paint.Style.FILL);
+
+        mCanvas.drawRect(new RectF(0, 0, mWidth, mHeight), mPaint);
+
+        // draw grid
+        mPaint.setColor(0x55FFFFFF);
+        for (int i = 0; i < mWidth; i = i + (int) (mWidth / 9)) {
+            mCanvas.drawLine(i, 0, i, mHeight, mPaint);
+        }
+        for (int j = 0; j < mHeight; j = j + (int) (mHeight / 5)) {
+            mCanvas.drawLine(0, j, mWidth, j, mPaint);
+
+        }
+
+        mReady = true;
+
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        synchronized (this) {
+
+            Iterator it = plots.entrySet().iterator();
+            while (it.hasNext()) {
+                Map.Entry m = (Map.Entry) it.next();
+                Plot p = (Plot) m.getValue();
+
+                mPaint.setStyle(Paint.Style.STROKE);
+                mPaint.setColor(p.color);
+                mPaint.setStyle(Paint.Style.STROKE);
+
+                int i = 0;
+
+                float y = 0;
+                float y0 = 0;
+                Path path = new Path();
+                for (int x = 0; x < mWidth; x += mDefinition) {
+
+
+                    y = CanvasUtils.map(p.plotValues.get(i), mMinBoundary, mMaxBoundary, mHeight, 0f);
+
+                    if (x == 0) {
+                        path.moveTo(x, y);
+                    }
+
+                    path.lineTo(x, y);
+
+                    if (i < mNumPoints - 1) {
+                        i++;
+                    }
+
+                }
+                canvas.drawPath(path, mPaint);
+
+
+                // show value
+                // mPaint.setColor(Color.BLACK);
+
+                // float lastVal =
+                // p.plotValues.get(plots.get(0).plotValues.size() - 1);
+                // canvas.drawText("" + lastVal, mWidth - 20, y, mPaint);
+
+            }
+
+            // draw border
+            // mPaint.setColor(0xFF000000);
+            // mPaint.setStyle(Style.STROKE);
+            // mCanvas.drawRoundRect(new RectF(0, 0, mWidth, mHeight), 0, 0,
+            // mPaint);
+
+        }
+    }
+
+    @ProtocoderScript
+    @APIMethod(description = "Init a plot name", example = "")
+    @APIParam(params = { "" })
+    public PPlotView init(String plotName) {
+
+        if (plots.containsKey(plotName) == false) {
+            plots.put(plotName, new Plot(Color.BLUE));
+        }
+        invalidate();
+
+        return this;
+    }
+
+
+    @ProtocoderScript
+    @APIMethod(description = "Updates the value of the plotName plot", example = "")
+    @APIParam(params = { "plotName", "value" })
+    public PPlotView update(String plotName, float v1) {
+
+        if (mReady) {
+            init(plotName);
+
+            // shift points
+            Plot p = plots.get(plotName);
+            p.plotValues.remove(0);
+            p.plotValues.add(v1);
+
+            // check if is min or max
+            if (v1 >= mMaxValue) {
+                mMaxValue = v1;
+            }
+            if (v1 < mMinValue) {
+                mMinValue = v1;
+            }
+
+            invalidate();
+        }
+
+        return this;
+    }
+
+    @ProtocoderScript
+    @APIMethod(description = "Updates the value of the default plot", example = "")
+    @APIParam(params = { "value" })
+    public PPlotView update(float v) {
+        update("default", v);
+
+        return this;
+    }
+
+    @ProtocoderScript
+    @APIMethod(description = "Changes the plot limits", example = "")
+    @APIParam(params = { "min", "max" })
+    public PPlotView setLimits(float min, float max) {
+        mMinBoundary = min;
+        mMaxBoundary = max;
+
+        return this;
+    }
+
+    @ProtocoderScript
+    @APIMethod(description = "Changes the plot definition", example = "")
+    @APIParam(params = { "definition" })
+    public PPlotView setDefinition(int definition) {
+        mDefinition = definition;
+        return this;
+    }
+
+    public void destroy() {
+        //if (bitmap != null) {
+        //	bitmap.recycle();
+        //}
+    }
+
+    public class Plot {
+
+        // values to plot
+        public Vector<Float> plotValues;
+        int color;
+
+        public Plot(int color) {
+            plotValues = new Vector<Float>();
+            this.color = color;
+
+            zero();
+        }
+
+        public void zero() {
+            plotValues.clear();
+            for (int i = 0; i < mNumPoints; i++) {
+                plotValues.add(0f);
+            }
+        }
+
+    }
+
+    @ProtocoderScript
+    @APIMethod(description = "Sets the line thickness", example = "")
+    @APIParam(params = { "thickness" })
+    public PPlotView setThickness(float r) {
+        thickness = r; // when dot
+        mPaint.setStrokeWidth(thickness); // when line
+        return this;
+    }
+
+    @ProtocoderScript
+    @APIMethod(description = "Changes the plot color", example = "")
+    @APIParam(params = { "plotName", "colorHex" })
+    public PPlotView setColor(String plotName, String rgb) {
+        new Color();
+        int c = Color.parseColor(rgb);
+
+        Plot p = plots.get(plotName);
+        if (p != null) {
+            p.color = c;
+        }
+
+        return this;
+    }
+
+    @ProtocoderScript
+    @APIMethod(description = "Get the plot array size", example = "")
+    @APIParam(params = { "" })
+    public int getSize() {
+        return mNumPoints;
+    }
+
+
+    @ProtocoderScript
+    @APIMethod(description = "Get the current values of the plot as an array", example = "")
+    @APIParam(params = { "plotName" })
+    public Float[] getArray(String plotName) {
+        Plot p = plots.get(plotName);
+        //MLog.d(TAG, "plot " + p);
+
+        return p.plotValues.toArray(new Float[p.plotValues.size()]);
+    }
+
+
+    @ProtocoderScript
+    @APIMethod(description = "Set an array of values in the plot", example = "")
+    @APIParam(params = { "plotName", "value" })
+    public PPlotView setArray(String plotName, float[] values) {
+        Plot p = plots.get(plotName);
+
+        MLog.d(TAG, p.plotValues.size() + " " + values.length);
+
+        for (int i = 0; i < values.length; i++) {
+            p.plotValues.set(i, values[i]);
+            // p.plotValues.clear();
+            // p.plotValues.set
+        }
+        invalidate();
+
+        return this;
+    }
 
 
     @ProtocoderScript
     @APIMethod(description = "Sets the background color", example = "")
     @APIParam(params = { "colorHex" })
-    public PlotView setBackground(String c) {
+    public PPlotView setBackground(String c) {
         super.setBackgroundColor(Color.parseColor(c));
 
         return this;
     }
 
-    @Override
-	@ProtocoderScript
-	@APIMethod(description = "Sets the line thickness", example = "")
-    @APIParam(params = { "thickness" })
-    public PlotView setThickness(float r) {
-		return super.setThickness(r);
-	}
 
-	@Override
-	@ProtocoderScript
-	@APIMethod(description = "Changes the plot definition", example = "")
-    @APIParam(params = { "definition" })
-    public PlotView setDefinition(int definition) {
-		return super.setDefinition(definition);
-	}
 
-	@Override
-	@ProtocoderScript
-	@APIMethod(description = "Changes the plot color", example = "")
-    @APIParam(params = { "plotName", "colorHex" })
-    public PlotView setColor(String plotName, String rgb) {
-		return super.setColor(plotName, rgb);
-	}
 
-	@Override
-	@ProtocoderScript
-	@APIMethod(description = "Changes the plot limits", example = "")
-    @APIParam(params = { "min", "max" })
-    public PlotView setLimits(float min, float max) {
-        return super.setLimits(min, max);
-	}
-
-	@ProtocoderScript
-	@APIMethod(description = "Updates the value of the default plot", example = "")
-    @APIParam(params = { "value" })
-    public void update(float value) {
-		super.setValue("default", value);
-	}
-
-	@ProtocoderScript
-	@APIMethod(description = "Updates the value of the plotName plot", example = "")
-    @APIParam(params = { "plotName", "value" })
-    public void update(String plotName, float value) {
-		super.setValue(plotName, value);
-	}
-
-	@ProtocoderScript
-	@APIMethod(description = "Set an array of values in the plot", example = "")
-    @APIParam(params = { "plotName", "value" })
-    public void setArray(String plotName, float[] values) {
-		super.setArray(plotName, values);
-	}
-
-	@ProtocoderScript
-	@APIMethod(description = "Get the current values of the plot as an array", example = "")
-    @APIParam(params = { "plotName" })
-    public Float[] getArray(String plotName) {
-		return super.getArray(plotName);
-	}
-
-	@ProtocoderScript
-	@APIMethod(description = "Get the plot array size", example = "")
-    @APIParam(params = { "" })
-    public int getSize() {
-		return super.getSize();
-	}
-
-	@ProtocoderScript
-	@APIMethod(description = "Init a plot name", example = "")
-    @APIParam(params = { "" })
-    public void init(String name) {
-		super.plotInit(name);
-	}
 
 
 }
