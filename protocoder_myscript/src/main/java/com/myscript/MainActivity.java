d06cbef0b4ae8bde494280b58ddfecb029a43137
José Juan Sánchez <josejuan.sanchez@gmail.com>
2015-03-26 21:36:59 +0100

    Cleaning a little bit

--- protocoder_myscript/src/main/java/com/myscript/MainActivity.java
+++ protocoder_myscript/src/main/java/com/myscript/MainActivity.java
@@ -12,6 +12,7 @@ import org.protocoderrunner.project.ProjectManager;
 
 /**
  * Created by victormanueldiazbarrales on 15/09/14.
+ * Recreated by @josejuansanchez on 26/03/15 :)
  */
 public class MainActivity extends AppRunnerActivity {
 
@@ -21,40 +22,13 @@ public class MainActivity extends AppRunnerActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        //AppSettings.STANDALONE = true;
-
-        // Load project from sdcard
-        if (AppSettings.STANDALONE == false) {
-
-            // Copy the project files from assets to sdcard
-            installMyScript();
-
-            // TODO: Wait until the script has been installed
-            // Possible solution: Could we use the callback "onReady"?
-            // Temporary solution: Use Thread.sleep(x)
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-        }
+        // Enable the standalone mode
+        AppSettings.STANDALONE = true;
 
         // Create a new project and inject it to the base class
         Project project = new Project(PROJECT_FOLDER, PROJECT_NAME);
         setProject(project);
 
         super.onCreate(savedInstanceState);
-    }
-
-    // Copy the project files from assets to sdcard
-    private void installMyScript() {
-        ProjectManager.getInstance().install(this,
-                ProjectManager.getInstance().FOLDER_MYSCRIPT,
-                new ProjectManager.InstallListener() {
-                    @Override
-                    public void onReady() {
-                    }
-                });
     }
 }
