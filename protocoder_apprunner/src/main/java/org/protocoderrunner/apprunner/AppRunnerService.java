84db1c5ba8f4f5156c19d365f4c468dbac7421af
Victor Diaz <victormdb@gmail.com>
2015-03-29 11:50:41 +0200

    adding all objects to service

--- protocoder_apprunner/src/main/java/org/protocoderrunner/apprunner/AppRunnerService.java
+++ protocoder_apprunner/src/main/java/org/protocoderrunner/apprunner/AppRunnerService.java
@@ -4,7 +4,17 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
+import org.protocoderrunner.apprunner.api.PApp;
+import org.protocoderrunner.apprunner.api.PBoards;
+import org.protocoderrunner.apprunner.api.PConsole;
+import org.protocoderrunner.apprunner.api.PDashboard;
 import org.protocoderrunner.apprunner.api.PDevice;
+import org.protocoderrunner.apprunner.api.PFileIO;
+import org.protocoderrunner.apprunner.api.PMedia;
+import org.protocoderrunner.apprunner.api.PNetwork;
+import org.protocoderrunner.apprunner.api.PProtocoder;
+import org.protocoderrunner.apprunner.api.PSensors;
+import org.protocoderrunner.apprunner.api.PUI;
 import org.protocoderrunner.apprunner.api.PUtil;
 import org.protocoderrunner.project.Project;
 import org.protocoderrunner.project.ProjectManager;
@@ -18,14 +28,58 @@ public class AppRunnerService extends Service {
     private AppRunnerInterpreter interp;
     private final String TAG = "AppRunnerService";
     private Project currentProject;
-    private PDevice pDevice;
-    private PUtil pUtil;
+
+    public PApp pApp;
+    public PBoards pBoards;
+    public PConsole pConsole;
+    public PDashboard pDashboard;
+    public PDevice pDevice;
+    public PFileIO pFileIO;
+    public PMedia pMedia;
+    public PNetwork pNetwork;
+    public PProtocoder pProtocoder;
+    public PSensors pSensors;
+    public PUI pUi;
+    public PUtil pUtil;
+
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         // Can be called twice
         interp = new AppRunnerInterpreter(this);
         interp.createInterpreter(false);
+
+        pApp = new PApp(this);
+        //pApp.initForParentFragment(this);
+        pBoards = new PBoards(this);
+        pConsole = new PConsole(this);
+        pDashboard = new PDashboard(this);
+        pDevice = new PDevice(this);
+        //pDevice.initForParentFragment(this);
+        pFileIO = new PFileIO(this);
+        pMedia = new PMedia(this);
+        //pMedia.initForParentFragment(this);
+        pNetwork = new PNetwork(this);
+        //pNetwork.initForParentFragment(this);
+        pProtocoder = new PProtocoder(this);
+        pSensors = new PSensors(this);
+        //pSensors.initForParentFragment(this);
+        pUi = new PUI(this);
+        //pUi.initForParentFragment(this);
+        pUtil = new PUtil(this);
+
+        interp.interpreter.addObjectToInterface("app", pApp);
+        interp.interpreter.addObjectToInterface("boards", pBoards);
+        interp.interpreter.addObjectToInterface("console", pConsole);
+        interp.interpreter.addObjectToInterface("dashboard", pDashboard);
+        interp.interpreter.addObjectToInterface("device", pDevice);
+        interp.interpreter.addObjectToInterface("fileio", pFileIO);
+        interp.interpreter.addObjectToInterface("media", pMedia);
+        interp.interpreter.addObjectToInterface("network", pNetwork);
+        interp.interpreter.addObjectToInterface("protocoder", pProtocoder);
+        interp.interpreter.addObjectToInterface("sensors", pSensors);
+        interp.interpreter.addObjectToInterface("ui", pUi);
+        interp.interpreter.addObjectToInterface("util", pUtil);
 
         String projectName = intent.getStringExtra(Project.NAME);
         String projectFolder = intent.getStringExtra(Project.FOLDER);
@@ -37,8 +91,8 @@ public class AppRunnerService extends Service {
         AppRunnerSettings.get().project = currentProject;
         String script = ProjectManager.getInstance().getCode(currentProject);
 
-        pDevice = new PDevice(this);
-        pUtil = new PUtil(this);
+
+
 
         interp.interpreter.addObjectToInterface("device", pDevice);
         interp.interpreter.addObjectToInterface("util", pUtil);
