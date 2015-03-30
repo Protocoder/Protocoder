ee8b7e7d971fd872ea5bb82a1f9c6690617f5b0e
Victor Diaz <victormdb@gmail.com>
2015-03-30 23:04:00 +0200

    trying out some drawing from the service

--- protocoder_apprunner/src/main/java/org/protocoderrunner/apprunner/AppRunnerService.java
+++ protocoder_apprunner/src/main/java/org/protocoderrunner/apprunner/AppRunnerService.java
@@ -1,9 +1,16 @@
 package org.protocoderrunner.apprunner;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.media.AudioManager;
 import android.os.IBinder;
+import android.view.Gravity;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
+import org.protocoderrunner.R;
 import org.protocoderrunner.apprunner.api.PApp;
 import org.protocoderrunner.apprunner.api.PBoards;
 import org.protocoderrunner.apprunner.api.PConsole;
@@ -16,9 +23,12 @@ import org.protocoderrunner.apprunner.api.PProtocoder;
 import org.protocoderrunner.apprunner.api.PSensors;
 import org.protocoderrunner.apprunner.api.PUI;
 import org.protocoderrunner.apprunner.api.PUtil;
+import org.protocoderrunner.events.Events;
 import org.protocoderrunner.project.Project;
 import org.protocoderrunner.project.ProjectManager;
 import org.protocoderrunner.utils.MLog;
+
+import de.greenrobot.event.EventBus;
 
 //stopService 
 //stopSelf 
@@ -41,6 +51,9 @@ public class AppRunnerService extends Service {
     public PSensors pSensors;
     public PUI pUi;
     public PUtil pUtil;
+
+    private WindowManager windowManager;
+    private ImageView img;
 
 
     @Override
@@ -98,6 +111,35 @@ public class AppRunnerService extends Service {
         interp.interpreter.addObjectToInterface("util", pUtil);
         interp.evalFromService(script);
 
+        //audio
+        //AudioManager audio = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+        //int currentVolume = audio.getStreamVolume(AudioManager.STREAM_MUSIC);
+        //this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
+
+
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+        img = new ImageView(this);
+        img.setImageResource(R.drawable.protocoder_icon);
+        img.setAlpha(0.5f);
+
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                //WindowManager.LayoutParams.WRAP_CONTENT,
+                //WindowManager.LayoutParams.WRAP_CONTENT,
+                50,
+                50,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+
+        params.gravity = Gravity.TOP | Gravity.LEFT;
+        params.x = 0;
+        params.y = 100;
+
+        windowManager.addView(img, params);
+
+        EventBus.getDefault().register(this);
+
         return Service.START_NOT_STICKY;
     }
 
@@ -116,5 +158,18 @@ public class AppRunnerService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
+
+        windowManager.removeView(img);
+        EventBus.getDefault().unregister(this);
     }
+
+
+    // execute lines
+    public void onEventMainThread(Events.ExecuteCodeEvent evt) {
+        String code = evt.getCode(); // .trim();
+        MLog.d(TAG, "event -> q " + code);
+
+        interp.evalFromService(code);
+    }
+
 }
