d62917d6f2a2cc86607da5e77397ea79c5750957
Victor Diaz <victormdb@gmail.com>
2015-04-03 20:15:43 +0200

    immersive mode reestablish after touch

--- protocoder_apprunner/src/main/java/org/protocoderrunner/base/BaseActivity.java
+++ protocoder_apprunner/src/main/java/org/protocoderrunner/base/BaseActivity.java
@@ -105,7 +105,13 @@ public class BaseActivity extends ActionBarActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
     }
 
     public void showHomeBar(boolean b) {
