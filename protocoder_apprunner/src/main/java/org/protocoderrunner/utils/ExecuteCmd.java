af61b946b0adea99871bf5f9cb106659ce19dc71
Victor Diaz <victormdb@gmail.com>
2015-04-03 20:13:03 +0200

    added fluid interface to executecmd

--- protocoder_apprunner/src/main/java/org/protocoderrunner/utils/ExecuteCmd.java
+++ protocoder_apprunner/src/main/java/org/protocoderrunner/utils/ExecuteCmd.java
@@ -33,13 +33,14 @@ import java.io.InputStreamReader;
 
 public class ExecuteCmd {
 
+    private static final String TAG = "ExecuteCmd";
+
     public interface ExecuteCommandCB {
         void event(String buffer);
     }
 
     private final String cmd;
     private final ExecuteCommandCB callbackfn;
-
 
     private Handler mHandler;
     private Thread mThread;
@@ -48,81 +49,85 @@ public class ExecuteCmd {
         this.cmd = cmd;
         this.callbackfn = callbackfn;
 
-        mThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Looper.prepare();
-
-                int count = 0;
-                String str = "";
-                try {
-                    final Process process = Runtime.getRuntime().exec(cmd);
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(
-                            process.getInputStream()));
-
-
-                    mHandler = new Handler() {
-                        @Override
-                        public void handleMessage(Message msg) {
-                            process.destroy();
-
-                            mThread.interrupt();
-                            mThread = null;
-
-                            try {
-                                process.waitFor();
-                            } catch (InterruptedException e) {
-
-                                e.printStackTrace();
-                            }
-                        }
-                    };
-
-
-                    int i;
-                    final char[] buffer = new char[4096];
-                    StringBuffer output = new StringBuffer();
-
-                    while ((i = reader.read(buffer)) > 0) {
-                        output.append(buffer, 0, i);
-
-                        Handler h = new Handler(Looper.getMainLooper());
-                        final int finalI = i;
-                        h.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                callbackfn.event(finalI + " " + String.valueOf(buffer));
-
-                            }
-                        });
-
-                    }
-                    reader.close();
-
-                    str = output.toString();
-                    // Log.d(TAG, str);
-                } catch (IOException e) {
-                    // Log.d(TAG, "Error");
-                    e.printStackTrace();
-                }
-                Looper.loop();
-            }
-
-        });
-
         WhatIsRunning.getInstance().add(this);
     }
 
-    public void start() {
+    private void initThread() {
+        mThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+            Looper.prepare();
+
+            int count = 0;
+            String str = "";
+            try {
+                //execute the command
+                final Process process = Runtime.getRuntime().exec(cmd);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(
+                        process.getInputStream()));
+
+                //handler that can stop the thread
+                mHandler = new Handler() {
+                    @Override
+                    public void handleMessage(Message msg) {
+                    process.destroy();
+
+                    mThread.interrupt();
+                    mThread = null;
+
+                    try {
+                        process.waitFor();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    }
+                };
+
+                //read the lines
+                int i;
+                final char[] buffer = new char[4096];
+                StringBuffer output = new StringBuffer();
+
+                while ((i = reader.read(buffer)) > 0) {
+                    output.append(buffer, 0, i);
+
+                    Handler h = new Handler(Looper.getMainLooper());
+                    final int finalI = i;
+                    h.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            callbackfn.event(finalI + " " + String.valueOf(buffer));
+                        }
+                    });
+
+                }
+                reader.close();
+
+                str = output.toString();
+            } catch (IOException e) {
+                // Log.d(TAG, "Error");
+                e.printStackTrace();
+            }
+            Looper.loop();
+            }
+
+        });
+    }
+
+    public ExecuteCmd start() {
+        initThread();
         mThread.start();
+
+        return this;
     }
 
     @ProtoMethod(description = "stop the running command", example = "")
-    public void stop() {
+    public ExecuteCmd stop() {
         Message msg = mHandler.obtainMessage();
         msg.arg1 = 0;
         mHandler.dispatchMessage(msg);
         // mHandler.
 
+        return this;
     }
 }
