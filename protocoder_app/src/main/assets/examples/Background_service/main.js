80fedeb0451773466f4eeda48a904cfef7069c0a
Victor Diaz <victormdb@gmail.com>
2015-04-30 19:05:21 +0200

    ready for 0.11.0

---
+++ protocoder_app/src/main/assets/examples/Background_service/main.js
@@ -0,0 +1,19 @@
+/*
+* This is an example of background processes.
+* Have in mind that is quite experimental (like whole Protocoder :P)
+*
+* Create a project appending "_service" to the name
+* such as MyProject_service
+* 
+* Protocoder will treat it automatically as a background service
+*
+* Currently only one service can be active. More than one might lead 
+* to some problems :)
+*
+*/
+
+
+util.delay(5000, function) {
+	device.vibrate(500);
+
+}).start();
