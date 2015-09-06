5f004139ea0d492e83507d95733be1eb4b0cd82a
Victor Diaz <victormdb@gmail.com>
2015-09-07 00:12:57 +0200

    first cosmetic changes

--- protocoder_app/src/main/java/org/protocoder/MainActivity.java
+++ protocoder_app/src/main/java/org/protocoder/MainActivity.java
@@ -24,6 +24,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -33,6 +34,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.FrameLayout;
 
@@ -64,11 +66,17 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+
+
         /*
          * Setup the ui
          */
         setupToolbar();
-        //addProjectFolderChooser(savedInstanceState);
+        addProjectFolderChooser(savedInstanceState);
         addProjectListFragment(savedInstanceState);
         //showIntroduction(savedInstanceState);
 
